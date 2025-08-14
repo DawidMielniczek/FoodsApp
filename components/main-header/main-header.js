8bc02acecbdad0c4eb68ee7d9b3d702432702b4f
Dawid <dawid.mielniczek99@gmail.com>
2025-08-14 18:00:52 +0200

    Recreate a header section

---
+++ components/main-header/main-header.js
@@ -0,0 +1,31 @@
+import logoImage from '@/assets/logo.png';
+import Link from 'next/link';
+import classes from './main-header.module.css';
+import Image from 'next/image';
+import MainHeaderBackground from './main-header-background';
+
+export default function MainHeader() {
+    return (
+        <>
+            <MainHeaderBackground />
+
+            <header className={classes.header}>
+                <Link href='/' className={classes.logo}>
+                    <Image src={logoImage} alt="A plate with food on it" priority />
+                    NextLevel Food
+                </Link>
+
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href="/meals"> Browse Meals </Link>
+                        </li>
+                        <li>
+                            <Link href="/community"> Foodies community </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
