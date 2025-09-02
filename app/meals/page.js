8cad3027d7974b4121b781aef234b997df7b8bd4
Dawid <dawid.mielniczek99@gmail.com>
2025-09-02 17:20:23 +0200

    Add sqlite db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,26 @@
-export default function CommunityPage(){
+import MealsGrid from '@/components/meals/meals-grid';
+import classes from './page.module.css';
+import Link from 'next/link';
+
+export default function MealsPage() {
     return (
-        <h1> Meals page </h1>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicius meals, created <span className={classes.highlight}> by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself.
+                </p>
+                <p className={classes.cta}>
+                    <Link href='/meals/share'>
+                        Share you favorite recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
     );
 }
