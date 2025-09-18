3438a44a385473b8e1dee8477dc3069ff81a8843
Dawid <dawid.mielniczek99@gmail.com>
2025-09-18 18:02:24 +0200

    Add image picker

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -31,7 +31,6 @@ export default function DetailMealsPage({params}){
             <p className={classes.instructions} dangerouslySetInnerHTML={{
                 __html: meal.instructions
             }}>
-
             </p>
         </main>
         </>
